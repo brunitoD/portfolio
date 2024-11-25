107d027a79da5be70cd041a41de2135ef3cf4069
brunitoD <168501394+brunitoD@users.noreply.github.com>
2024-11-25 14:30:28 -0300

    agregados

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 "use strict";
 const menu = document.querySelector(".menu-hamburguesa");
 const lista = document.querySelector(".lista");
+const enlaces = document.querySelectorAll(".lista a");
 if (menu && lista) {
     menu.addEventListener("click", () => {
         if (lista.classList.contains("active")) {
             lista.classList.remove("active");
             lista.classList.add("invisible");
+            console.log(lista.className);
         }
         else {
             lista.classList.remove("invisible");
             lista.classList.add("active");
         }
+        enlaces.forEach(enlace => {
+            enlace.addEventListener("click", () => {
+                console.log(lista.className);
+                if (lista.classList.contains("active")) {
+                    lista.classList.remove("active");
+                    lista.classList.add("invisible");
+                    console.log(lista.className)
+                }
+            });
+        });
     });
 }
--- main.ts
+++ main.ts
@@ -1,14 +1,30 @@
 const menu = document.querySelector<HTMLDivElement>(".menu-hamburguesa");
 const lista = document.querySelector<HTMLDivElement>(".lista");
+const enlaces = document.querySelectorAll<HTMLAnchorElement>(".lista a")
 
 if (menu && lista) {
   menu.addEventListener("click", () => {
     if (lista.classList.contains("active")) {
       lista.classList.remove("active");
       lista.classList.add("invisible");
+      console.log(lista.className)
+
     } else {
       lista.classList.remove("invisible");
       lista.classList.add("active");
     }
+    enlaces.forEach(enlace =>{
+      enlace.addEventListener("click", ()=>{
+        console.log(lista.className)
+        if(lista.classList.contains("active")){
+             lista.classList.remove("active")
+             lista.classList.add("invisible")
+        }else{
+           console.log(lista.className)
+        }
+      })
+    })
   });
+
+
 }
